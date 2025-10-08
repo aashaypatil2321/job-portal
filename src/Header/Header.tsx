d481b4a8140aee8f19f627b91c7d521e90668bbc
aashaypatil2321 <aashaypatil2321@gmail.com>
2025-10-09 01:00:44 +0530

    Header and Navbar completed

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import {IconAsset } from "@tabler/icons-react"
+import React from "react";
+import {
+  IconAnchor,
+  IconAsset,
+  IconBell,
+  IconSettings,
+} from "@tabler/icons-react";
+import { Avatar, Indicator } from "@mantine/core";
+import NavLinks from "./NavLinks";
 
 const Header = () => {
   return (
-    <div className='w-full bg-black h-28 flex text-white justify-between px-6 items-center'>
-        <div>
-<IconAsset />
-icon
+    <div className="w-full bg-mine-shaft-950 h-20 flex text-white justify-between px-6 items-center ">
+      <div className="flex gap-1 items-center text-bright-sun-400">
+        <IconAnchor className="h-10 w-8" stroke={2.5} />
+        <div className="text-3xl font-semibold ">JobHook</div>
+      </div>
+     {NavLinks() }
+      <div className="flex gap-3 items-center">
+        <div className="flex gap-2 items-center">
+          <div>marshal</div>
+          <Avatar src="" alt="its image here" />
         </div>
-        <div>
-links
+        <div className="bg-mine-shaft-900 p-1.5 rounded-full ">
+          <IconSettings stroke={1.5} />
         </div>
-        <div>
-profile
+        <div className="bg-mine-shaft-900 p-1.5 rounded-full">
+          <Indicator color="brightSun.4" size={9} processing offset={6}>
+            <IconBell stroke={1.5} />
+          </Indicator>
         </div>
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,9 +4,9 @@ import Header from '../Header/Header'
 
 const HomePage = () => {
   return (
-   <>
+   <div className='min-h-[100vh] bg-mine-shaft-950 font-["poppins"]'>
     <Header />
-   </>
+   </div>
   )
 }
 
