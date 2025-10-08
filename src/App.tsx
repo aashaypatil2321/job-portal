94f43400730a5daff4cb064366c820ece7436dd9
aashaypatil2321 <aashaypatil2321@gmail.com>
2025-10-08 20:25:43 +0530

    installation and setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <MantineProvider>
+  <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+   </MantineProvider>
   );
 }
 
